481a000b7b4a2c7ba3332759b1da717039a980ea
Tom Zehle <t.zehle@gmail.com>
2023-02-01 23:28:35 +0100

    init of matrix calcs, aber funktioniert evtl noch nicht

---
+++ src/js/table.js
@@ -0,0 +1,39 @@
+// class Table {
+//     constructor() {
+//         this.html = `<table>
+//         <tr> <h1>A = </h1></tr>
+//         <tr>
+//             <td><input type="number" name="b11" value="0"></td>
+//             <td><input type="number" name="b12" value="0"></td>
+//             <td><input type="number" name="b13" value="0"></td>
+//         </tr>
+//         <tr>
+//             <td><input type="number" name="b21" value="0"></td>
+//             <td><input type="number" name="b22" value="0"></td>
+//             <td><input type="number" name="b23" value="0"></td>
+//         </tr>
+//         <tr>
+//             <td><input type="number" name="b31" value="0"></td>
+//             <td><input type="number" name="b32" value="0"></td>
+//             <td><input type="number" name="b33" value="0"></td>
+//         </tr>
+//         <button>Add Row</button>
+//         <button>Add Column</button>
+//         <button>Remove Row</button>
+//         <button>Remove Column</button>
+//         `;
+//     this.endOfHtml = "</table>";
+//     }
+
+//     addRow() {
+//         this.html += `<tr>
+//         <td><input type="number" name="b11" value="0"></td>
+//         <td><input type="number" name="b12" value="0"></td>
+//         <td><input type="number" name="b13" value="0"></td>
+//     </tr>`;
+//     }
+
+//     getHtml() {
+//         return this.html + this.endOfHtml;
+//     }
+// }
