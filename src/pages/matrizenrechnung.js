4df0e05c3cde2ac0fb00519540bdb935685de799
Tom Zehle <t.zehle@gmail.com>
2023-02-03 00:01:13 +0100

    Tidied Up of Code

--- src/pages/matrizenrechnung.js
+++ src/pages/matrizenrechnung.js
@@ -1,54 +1,39 @@
+
 class Table {
     constructor() {
         this.tableElement = document.createElement("table");
         this.tableBody = document.createElement("tbody");
         this.tableBody.id = "tableRows";
         this.tableElement.appendChild(this.tableBody);
-
-        this.rows = [];
+    
         this.nColumns = 3;
-        for (let i = 0; i < 3; i++) {
-            this.addRow();
-        }
+        this.rows = [];
+        for (let i = 0; i < 3; i++){this.addRow();}
+    
+        const buttons = [
+            { name: "Add Row", id: "RowAdder", function: this.addRow },
+            { name: "Add Column", id: "ColumnAdder", function: this.addColumn },
+            { name: "Remove Row", id: "RowRemover", function: this.removeRow },
+            { name: "Remove Column", id: "ColumnRemover", function: this.removeColumn },
+            { name: "Save", id: "Saver", function: this.getData }
+        ];
+    
+        const buttonsContainer = document.createElement("div");
+        buttonsContainer.id = "buttons";
 
-        this.rowAdder = document.createElement("button");
-        this.rowAdder.textContent = "Add Row";
-        this.rowAdder.id = "RowAdder";
-        this.rowAdder.addEventListener("click", this.addRow.bind(this));
-
-        this.columnAdder = document.createElement("button");
-        this.columnAdder.textContent = "Add Column";
-        this.columnAdder.id = "ColumnAdder";
-        this.columnAdder.addEventListener("click", this.addColumn.bind(this));
-
-        this.rowRemover = document.createElement("button");
-        this.rowRemover.textContent = "Remove Row";
-        this.rowRemover.id = "RowRemover";
-        this.rowRemover.addEventListener("click", this.removeRow.bind(this));
-
-        this.columnRemover = document.createElement("button");
-        this.columnRemover.textContent = "Remove Column";
-        this.columnRemover.id = "ColumnRemover";
-        this.columnRemover.addEventListener("click", this.removeColumn.bind(this));
-
-        this.saver = document.createElement("button");
-        this.saver.textContent = "Save";
-        this.saver.id = "Saver";
-        this.saver.addEventListener("click", this.getData.bind(this));
-
-
-        const buttons = document.createElement("div");
-        buttons.id = "buttons";
-        buttons.appendChild(this.rowAdder);
-        buttons.appendChild(this.columnAdder);
-        buttons.appendChild(this.rowRemover);
-        buttons.appendChild(this.columnRemover);
-        buttons.appendChild(this.saver);
-
+        buttons.forEach(button => {
+            const buttonElement = document.createElement("button");
+            buttonElement.textContent = button.name;
+            buttonElement.id = button.id;
+            buttonElement.addEventListener("click", button.function.bind(this));
+            buttonsContainer.appendChild(buttonElement);
+        });
+    
         this.tableContainer = document.createElement("div");
         this.tableContainer.appendChild(this.tableElement);
-        this.tableContainer.appendChild(buttons);
+        this.tableContainer.appendChild(buttonsContainer);
     }
+      
 
     addRow() {
         if (this.rows.length > 10) {
@@ -61,9 +46,7 @@ class Table {
         for (let i = 0; i < this.nColumns; i++) {
             const cell = document.createElement("td");
             const input = document.createElement("input");
-            // input.type = "number";
             input.value = "";
-            // set input size
             input.size = 2;
             input.id = `${rowId}-${i}`;
             cell.appendChild(input);
@@ -79,7 +62,8 @@ class Table {
         for (let i = 0; i < this.rows.length; i++) {
             const cell = document.createElement("td");
             const input = document.createElement("input");
-            input.type = "number";
+            // input.type = "ber";
+            input.size = 2;
             input.value = "";
             input.id = `${i}-${this.nColumns - 1}`;
             cell.appendChild(input);
@@ -135,17 +119,16 @@ console.log("Starting webpage")
 let tableObj = new Table();
 document.getElementById("table").appendChild(tableObj.getHtml());
 
-// focus first cell when i press space
+
 document.addEventListener("keydown", function(e) {
-    // current active cell
     let activeCellId = document.activeElement.id;
     console.log(activeCellId);
     if (activeCellId == "") {
-        return;// document.getElementById("0-0").focus();
+        return;
     }
-    // row
+
     let row = Number(activeCellId.split("-")[0]);
-    // column
+
     let column = Number(activeCellId.split("-")[1]);
 
     if (e.code == "ArrowUp") {
@@ -153,113 +136,22 @@ document.addEventListener("keydown", function(e) {
             document.getElementById(`${row - 1}-${column}`).focus();
         }
     }
-    // arrowDown
-    if (e.code == "ArrowDown") {
+    
+    else if (e.code == "ArrowDown") {
         if (row < tableObj.rows.length - 1) {
             document.getElementById(`${row + 1}-${column}`).focus();
         }
     }
-    // arrowLeft
-    if (e.code == "ArrowLeft") {
+    
+    else if (e.code == "ArrowLeft") {
         if (column > 0) {
             document.getElementById(`${row}-${column - 1}`).focus();
         }
     }
-    // arrowRight
-    if (e.code == "ArrowRight") {
+    
+    else if (e.code == "ArrowRight") {
         if (column < tableObj.nColumns - 1) {
             document.getElementById(`${row}-${column + 1}`).focus();
         }
     }
 });
-// // import Table from "../table.js";
-
-// class Table {
-//     constructor() {
-//         this.html = `<h1>A=</h1>
-//         <table>
-//         <tbody id="tableRows">`
-
-//         this.rows = []
-//         this.nColumns = 3;
-//         for (let i = 0; i < 3; i++) {
-//             this.addRow()
-//         }
-//         this.endOfHtml = `</tbody></table>
-//         <div id="buttons">    
-//             <button id="RowAdder">Add Row</button>
-//             <button id="ColumnAdder">Add Column</button>
-//             <button id="RowRemover">Remove Row</button>
-//             <button id="ColumnRemover">Remove Column</button>
-//         </div>`;
-//     }
-
-//     addRow() {
-//         let rowId = this.rows.length;
-//         let extraRow = [`<tr id=${rowId}>`];
-//         // extraRow.push();
-//         for (let i = 0; i < this.nColumns; i++) {
-//             extraRow.push(`<td><input type="number" value="" id=${rowId}></td>`);
-//         }
-//         extraRow.push("</tr>");
-//         let extraRowString = extraRow.join("");
-//         this.rows.push(extraRowString);
-//         return extraRowString;
-//     }
-
-//     addColumn() {
-//         this.nColumns += 1;
-//         for (let i = 0; i < this.rows.length; i++) {
-//             this.rows[i] = this.rows[i].slice(0, -5) + `<td><input type="number" value="" id=${i}></td></tr>`;
-//         }
-
-//     }
-
-//     removeRow() {
-//         if (this.rows.length > 1){
-//             this.rows.pop();
-//         }
-//     }
-
-//     removeColumn() {
-//         this.nColumns = Math.max(1, this.nColumns - 1);
-//         for (let i = 0; i < this.rows.length; i++) {
-//             this.rows[i] = this.rows[i].slice(0, -22) + "</tr>";
-//         }
-//     }
-
-//     getHtml() {
-//         return this.html + this.rows.join("") + this.endOfHtml;
-//     }
-
-//     readTable() {
-//         let table = document.getElementById("table");
-//         console.log(table);
-//     }
-// }
-
-// console.log("Starting webpage")
-// let tableObj = new Table();
-// document.getElementById("table").innerHTML = tableObj.getHtml();
-
-// document.getElementById("RowAdder").addEventListener("click", function() {
-//     let newRow = tableObj.addRow();
-//     document.getElementById("tableRows").innerHTML = tableObj.rows.join("");
-//     // document.getElementById("tableRows").insertAdjacentHTML("beforeend", newRow);
-// });
-
-// document.getElementById("ColumnAdder").addEventListener("click", function() {
-//     tableObj.addColumn();
-//     document.getElementById("tableRows").innerHTML = tableObj.rows.join("");
-// });
-
-// document.getElementById("RowRemover").addEventListener("click", function() {
-//     tableObj.removeRow();
-//     document.getElementById("tableRows").innerHTML = tableObj.rows.join("");
-// });
-
-// document.getElementById("ColumnRemover").addEventListener("click", function() {
-//     tableObj.removeColumn();
-//     document.getElementById("tableRows").innerHTML = tableObj.rows.join("");
-// });
-
