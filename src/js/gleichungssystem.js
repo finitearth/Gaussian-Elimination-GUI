a873b2eb4274b89b1438299808938bf332cb0cbf
Tom Zehle <t.zehle@gmail.com>
2023-02-15 13:35:55 +0100

    timooooooooo

--- src/js/gleichungssystem.js
+++ src/js/gleichungssystem.js
@@ -9,11 +9,9 @@ function addTable() {
 
 function calculateSolution() {
     let coefMatrix = tables[0].getData();
-    console.log(coefMatrix.stringify());
     let solMatrix = tables[1].getData();
-    console.log(solMatrix.stringify());
+
     solMatrix = gaussElimination(coefMatrix, solMatrix);
-    console.log(solMatrix.stringify());
     tables[2].setData(solMatrix.array);
 }
 
