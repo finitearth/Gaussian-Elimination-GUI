992654cbad611207448d4167a0c6706dae1f987a
Tom Zehle <t.zehle@gmail.com>
2023-02-23 17:24:26 +0100

    add error cases

--- src/js/gaussalgorithm.js
+++ src/js/gaussalgorithm.js
@@ -1,4 +1,11 @@
+import { UnsolvableMatrixException, DivByZeroException } from "./exceptions.js";
+
 export function gaussElimination(coefMatrix, solMatrix) {
+    // check for rows = cols in coefmatrix
+    if (coefMatrix.nRows !== coefMatrix.nCols) {
+        throw new UnsolvableMatrixException();
+    }
+    let invPivot;
     for (let i = 0; i < coefMatrix.nRows; i++) {
         let [pivotIndex, pivotElement] = coefMatrix.getPivot(i);
 
@@ -6,8 +13,17 @@ export function gaussElimination(coefMatrix, solMatrix) {
         coefMatrix = coefMatrix.swapRows(i, pivotIndex);
         solMatrix = solMatrix.swapRows(i, pivotIndex);
 
-        // Scale the row so that the pivot element is 1
-        let invPivot = pivotElement.inverse();
+        try {
+            // Scale the row so that the pivot element is 1
+            invPivot = pivotElement.inverse();
+        } catch (e) {
+            if (e instanceof DivByZeroException) {
+                // Matrix is singular
+                throw new UnsolvableMatrixException();
+            } else {
+                throw e;
+            }
+        }
         coefMatrix = coefMatrix.multiplyRowByScalar(i, invPivot);
         solMatrix = solMatrix.multiplyRowByScalar(i, invPivot);
 
