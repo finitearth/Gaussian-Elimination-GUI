8c20d3f8f5fa02dbb9f9d330a237a3014dfef43e
timo282 <ti-heiss@t-online.de>
2023-03-01 00:28:15 +0100

    Add modal

---
+++ src/js/modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("help-modal");
+var btnOpen = document.getElementById("btn-help-modal");
+var spanClose = document.getElementById("span-close");
+var btnClose = document.getElementById("btn-close");
+
+btnOpen.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+spanClose.addEventListener("click", closeModal);
+btnClose.addEventListener("click", closeModal);
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
