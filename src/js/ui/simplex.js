8eaa7c74cba3cf88fca9d95133bfd42e28282265
Tom Zehle <t.zehle@gmail.com>
2023-08-21 00:21:51 +0200

    simplex anpassungen

--- src/js/ui/simplex.js
+++ src/js/ui/simplex.js
@@ -1,9 +1,8 @@
-import { Table } from "../intermediate/table.js";
-import { Matrix } from "../logic/matrix.js";
+import { Table, addKeyDownListener } from "../intermediate/table.js";
 import { setEventListenerFunction } from "../intermediate/eventlisteners.js";
 import { simplexAlgorithm } from "../logic/simplexAlgorithm.js";
 import { listenTableDimension } from "../intermediate/eventlisteners.js";
-import { Fraction, ONE, ZERO } from "../logic/fraction.js";
+import { getById } from "../intermediate/getElement.js";
 
 // =========== Tables ===========
 let coefTable = new Table("coef-table", 3);
@@ -14,47 +13,45 @@ let rhsTable = new Table("rhs-table", 1);
 rhsTable.addColumnDescription("b");
 rhsTable.removeBrackets();
 
-coefTable.setData(
-    new Matrix([
-        [new Fraction(4, 10), new Fraction(3, 5), ONE, ZERO, ZERO],
-        [new Fraction(3, 1), ONE, ZERO, ONE, ZERO],
-        [new Fraction(3, 1), new Fraction(6, 1), ZERO, ZERO, ONE],
-        [new Fraction(-990, 1), new Fraction(-900, 1), ZERO, ZERO, ZERO],
-    ])
-);
-
-rhsTable.setData(
-    new Matrix([
-        [new Fraction(17, 2)],
-        [new Fraction(25, 1)],
-        [new Fraction(70, 1)],
-        [ZERO],
-    ])
-);
-
-// let constraintCoefTable = new Table("constraint-coef-table");
-// constraintCoefTable.removeBrackets();
-// constraintCoefTable.addRowDescription();
-// constraintCoefTable.addColumnDescription("x");
-// let objectiveCoefTable = new Table("objective-coef-table", 3);
-// objectiveCoefTable.setNRows(1);
-// objectiveCoefTable.removeBrackets();
-// objectiveCoefTable.addRowDescription("f");
-// objectiveCoefTable.addColumnDescription("0*");
-
-// let objectiveResultTable = new Table("objective-b-table", 1);
-// objectiveResultTable.setNRows(1);
-// objectiveResultTable.removeBrackets();
-
 let outTable = new Table("output-table", 3);
-outTable.setNRows(3);
 outTable.disableInput();
+outTable.removeBrackets();
 
 let outBTable = new Table("output-b-table", 1);
-outBTable.setNRows(3);
 outBTable.disableInput();
+outBTable.removeBrackets();
+
+// coefTable.setData(
+//     new Matrix([
+//         [new Fraction(4, 10), new Fraction(3, 5), ONE, ZERO, ZERO],
+//         [new Fraction(3, 1), ONE, ZERO, ONE, ZERO],
+//         [new Fraction(3, 1), new Fraction(6, 1), ZERO, ZERO, ONE],
+//         [new Fraction(-990, 1), new Fraction(-900, 1), ZERO, ZERO, ZERO],
+//     ])
+// );
+
+// rhsTable.setData(
+//     new Matrix([
+//         [new Fraction(17, 2)],
+//         [new Fraction(25, 1)],
+//         [new Fraction(70, 1)],
+//         [ZERO],
+//     ])
+// );
+
+
 
 // =========== Event listeners ===========
+addKeyDownListener([coefTable, rhsTable], true);
+
+let conversionButtonchecked = false;
+getById("button-representation-conversion").addEventListener("click", () => {
+    conversionButtonchecked = !conversionButtonchecked;
+    [outTable, outBTable].forEach(table => {
+        table.convertRepresentation(conversionButtonchecked);
+    });
+});
+
 setEventListenerFunction(
     "button-calculate",
     [coefTable, rhsTable],
@@ -70,7 +67,7 @@ listenTableDimension(
     false,
     null,
     false,
-    2
+    3
 );
 listenTableDimension(
     "input-nr-cols",
@@ -80,13 +77,5 @@ listenTableDimension(
     false,
     null,
     false,
-    2
+    3
 );
-// listenTableDimension("input-nr-cols", [], [], "cols", false, null, false, 1);
-// listenTableDimension("input-nr-cols", [], [], "cols", false, null, false, 2);
-
-// let validPattern = /^[-+]?[\d]*[.,\/]?[\d]*$/;
-
-// getById("input-table-placeholder").addEventListener("keydown", () => {
-//     validate(validPattern)
-// });
